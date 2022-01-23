2d2a1171e89b956b15697ad214a45a729fb56176
MichealGSSmith <w0777761@myscc.ca>
2022-01-22 23:38:50 -0500

    some issues but some progress
    So the css is broken, i think that why i had the content.wrapper but i will work on that some more later, I kinda have the js working, its now selecting through all the cells, but it makes the first go away but then after that it just brings in the next cell without deleting the previous one lol well we get to go deeper!!!

--- Click Here Game/JS/site.js
+++ Click Here Game/JS/site.js
@@ -3,17 +3,18 @@ $(document).ready(function() {
     //when clicking the visible section this will make it go hidden
     $('section').click(function() {
 
+
         //this is changing the first one from visible to hidden
-        let elem = $('.visible');
+        let elem = $('section.visible');
         elem.addClass('hidden');
         elem.removeClass('visible');
         //just seeing if it clicking or not
         console.log('it clicking');
         //adding class visible and removing hidden from the next one in line
         elem.next('section.item').toggle(function(){
-            $(this).addClass('visible');
+            $(this).removeClass('hidden');$(this).addClass('visible');
         }, function() {
-            $(this).removeClass('hidden');
+            ;$(this).addClass('visible');
         });
         
         
